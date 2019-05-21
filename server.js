f69839aef38d741fc7444db27e2050fa76a964f4
Feng Lou <fenglou@FengdeMacBook-Pro.local>
2019-05-21 21:50:52 +0800

    test

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require ("express");
+const app = express();
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile( __dirname + "/index.html")
+});
+
+app.post("/",function(req,res){
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var num3 = num1 + num2;
+  res.send("The result is" + num3);
+})
+
+app.listen(3000,function(){
+  console.log("hello the world");
+})
